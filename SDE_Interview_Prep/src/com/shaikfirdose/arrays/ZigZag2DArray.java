d708097fcbbc0515b56998b837a1c99d620f9fb6
Firdose <shaikfirdose6652+github@gmail.com>
2023-10-21 22:45:52 +0530

    ZigZag 2D array problem added

---
+++ SDE_Interview_Prep/src/com/shaikfirdose/arrays/ZigZag2DArray.java
@@ -0,0 +1,33 @@
+package com.shaikfirdose.arrays;
+
+import java.util.Scanner;
+
+public class ZigZag2DArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+        int[][] arr = new int[row][column];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<row;i++){
+            if(i%2==0){
+                for(int j=0;j<column;j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            else{
+                for(int j=column-1;j>=0;j--){
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
+
+	}
+
+}
