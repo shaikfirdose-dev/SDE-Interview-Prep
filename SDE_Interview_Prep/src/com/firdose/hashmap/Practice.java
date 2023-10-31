a20836ddeb8636ed8fc1b8722f270d331e042041
Firdose <shaikfirdose6652+github@gmail.com>
2023-10-31 23:23:20 +0530

    Practice problem added

---
+++ SDE_Interview_Prep/src/com/firdose/hashmap/Practice.java
@@ -0,0 +1,29 @@
+package com.firdose.hashmap;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+	    int n= sc.nextInt();
+	    int[] x =new int[n];
+	    int[] y =new int[n];
+	    for(int i=0;i<n;i++){
+	        x[i] = sc.nextInt();
+	    }
+	    for(int i=0;i<n;i++){
+	        y[i] = sc.nextInt();
+	    }
+	    
+	    HashSet<String> hs = new HashSet<>();
+	    for(int i=0;i<n;i++){
+	        hs.add(x[i]+" "+y[i]);
+	    }
+	    System.out.println(hs.size());
+
+	}
+
+}
