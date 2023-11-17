c2f85f310ca4e5af6c22c4f9b947d55e7ebdb057
Firdose <shaikfirdose6652+github@gmail.com>
2023-11-17 21:37:38 +0530

    Delete node in a linked list added

---
+++ SDE_Interview_Prep/src/com/firdose/linkedlist/DeleteNode.java
@@ -0,0 +1,14 @@
+
+//Leetcode 237:  https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+package com.firdose.linkedlist;
+
+public class DeleteNode {
+	
+	
+	public void deletenode(Node node) { 
+		node.val = node.next.val;     // here changing current node value with next node value
+		node.next = node.next.next; 	// here updating current reference with next next node
+	}
+
+}
