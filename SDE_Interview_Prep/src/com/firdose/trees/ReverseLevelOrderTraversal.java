55f0ed2fbc1aad1216e677c4eb7fe0d141845314
Firdose <shaikfirdose6652+github@gmail.com>
2023-12-01 00:15:14 +0530

    Updared reverse level order

--- SDE_Interview_Prep/src/com/firdose/trees/ReverseLevelOrderTraversal.java
+++ SDE_Interview_Prep/src/com/firdose/trees/ReverseLevelOrderTraversal.java
@@ -1,5 +1,41 @@
 package com.firdose.trees;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
 public class ReverseLevelOrderTraversal {
+
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		List<List<Integer>> ans = new ArrayList<>();
+
+		if (root == null) {
+			return ans;
+		}
+		queue.add(root);
+
+		while (queue.size() > 0) {
+			int n = queue.size();
+			List<Integer> elements = new ArrayList<>();
+			for (int i = 1; i <= n; i++) {
+				TreeNode temp = queue.remove();
+				elements.add(temp.val);
+				if (temp.left != null) {
+					queue.add(temp.left);
+				}
+				if (temp.right != null) {
+					queue.add(temp.right);
+				}
+
+			}
+			ans.add(elements);
+		}
+		Collections.reverse(ans);
+		return ans;
+
+	}
 
 }
