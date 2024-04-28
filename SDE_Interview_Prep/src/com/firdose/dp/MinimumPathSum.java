3815eab99f39838efe4796e79d194c839c62c318
Shaik Firdose <shaikfirdose6652+github.com>
2024-04-28 21:48:23 +0530

    dp practiced

--- SDE_Interview_Prep/src/com/firdose/dp/MinimumPathSum.java
+++ SDE_Interview_Prep/src/com/firdose/dp/MinimumPathSum.java
@@ -7,6 +7,41 @@ package com.firdose.dp;
 
 public class MinimumPathSum {
 	public int minPathSum(int[][] grid) {
+		
+		//iterative code
+		/*
+		 * T.C:O(M*N)
+		 * S.C:O(M*N)
+		 */
+        // int m = grid.length;
+        // int  n = grid[0].length;
+        // int[][] dp = new int[m][n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0){
+        //             dp[i][j] = grid[i][j];
+        //         }
+        //         else if(i==0){
+        //             dp[i][j] = dp[i][j-1]+grid[i][j];
+        //         }
+        //         else if(j==0){
+        //             dp[i][j] = dp[i-1][j]+grid[i][j];
+        //         }
+        //         else{
+        //             dp[i][j] = Math.min(dp[i][j-1],dp[i-1][j])+grid[i][j];
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+		
+		
+		/*
+		 * 
+		 * Recursive code
+		 * T.C:O(M*N)
+		 * S.C:O(M*N)
+		 */
 		int m = grid.length;
 		int n = grid[0].length;
 		int dp[][] = new int[m][n];
